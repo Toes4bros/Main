9f46c44eb1a531389b005f5c91f53516d17b8fca
Toes4bros <toesbros805@gmail.com>
2023-02-12 08:44:34 -0800

    modified:   script.js

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const image = document.getElementById("image");
 
 let currentImageIndex = 0;
 const images = [
-  "https://cdn.pixabay.com/photo/2017/10/02/07/12/nude-2808070__340.jpg",
-  "https://cdn.pixabay.com/photo/2013/05/30/18/21/cat-114782__340.jpg",
-  "https://cdn.pixabay.com/photo/2017/07/28/16/22/erotic-2549106__340.jpg",
-  "https://cdn.pixabay.com/photo/2016/10/05/14/40/legs-1716821__340.jpg",
-  "https://cdn.pixabay.com/photo/2017/07/28/12/56/legs-2548600__340.jpg",
-  "https://cdn.pixabay.com/photo/2017/07/31/09/48/woman-2557063__340.jpg",
-  "https://cdn.pixabay.com/photo/2020/03/05/18/42/feet-4905180__340.jpg",
-  "https://cdn.pixabay.com/photo/2022/11/03/07/09/barefeet-7566810__340.jpg"
+  "https://cdn.pixabay.com/photo/2020/06/22/08/27/cat-5328304__340.jpg",
+  "https://cdn.pixabay.com/photo/2016/05/02/12/58/tiger-1367197__340.jpg",
+  "https://cdn.pixabay.com/photo/2016/05/06/12/23/cats-paw-1375792__340.jpg",
+  "https://cdn.pixabay.com/photo/2016/11/14/20/52/cat-1824668__340.jpg",
+  "https://cdn.pixabay.com/photo/2016/11/22/17/37/cat-1849854__340.jpg",
+  "https://cdn.pixabay.com/photo/2018/04/07/09/13/chartreux-3298051__340.jpg",
+  "https://cdn.pixabay.com/photo/2017/01/07/14/33/cat-1960537__340.jpg",
+  "https://cdn.pixabay.com/photo/2016/09/25/14/41/cats-paws-1693839__340.jpg"
   
 ];
 
